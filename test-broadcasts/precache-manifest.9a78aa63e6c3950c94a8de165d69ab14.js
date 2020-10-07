d14847940275780248ee84bef838f6372c3e0d3c
musovvir <mr.muzagov@mail.ru>
2020-10-07 15:49:59 +0300

    add test

---
+++ test-broadcasts/precache-manifest.9a78aa63e6c3950c94a8de165d69ab14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb2f95ce77fb41661e89215335279e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aafc8b09bc716e947a5f",
+    "url": "./static/css/main.f32c9fd4.chunk.css"
+  },
+  {
+    "revision": "bf2bf66aa60c782682fa",
+    "url": "./static/js/2.f51d7eda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.f51d7eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aafc8b09bc716e947a5f",
+    "url": "./static/js/main.f651b618.chunk.js"
+  },
+  {
+    "revision": "d9d8025f1ee7de5d2a86",
+    "url": "./static/js/runtime-main.59374127.js"
+  }
+]);
